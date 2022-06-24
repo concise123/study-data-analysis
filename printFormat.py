9c1bb83978cdba075286da7ef466d68688bbbca1
concise123 <kang011235@gmail.com>
2022-06-25 01:17:35 +0900

    print함수 포맷과 함께 사용 연습

---
+++ printFormat.py
@@ -0,0 +1,7 @@
+num1 = 1
+num2 = 2
+str1 = "합"
+str2 = '곱'
+print(f'{num1}과 {num2}의 {str1}은 {num1 + num2}입니다.')
+print('{}과 {}의 {}은 {}입니다.'.format(num1, num2, str1, num1 + num2))
+print('%d과 %d의 %s는 %d입니다.' % (num1, num2, str1, num1 + num2))
